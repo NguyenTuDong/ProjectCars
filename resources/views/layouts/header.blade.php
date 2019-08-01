2ecfed09fc57b8ce661ce2903d9fec8963f8e891
Dong Tu Nguyen <tudong.ng@gmail.com>
2019-08-01 22:53:12 +0700

    test

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Mua bán ô tô, xe hơi, ô tô cũ giá rẻ</title>
