b8ba361c6a9497f926542b99f5604456e4b6c27d
Dong Tu Nguyen <tudong.ng@gmail.com>
2019-08-01 22:55:31 +0700

    test

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="author" content="Nguyen Tu Dong">
 
     <title>Mua bán ô tô, xe hơi, ô tô cũ giá rẻ</title>
 
