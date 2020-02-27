4abb76467a29fb0bd1f7f18fd2babdd9fd611041
tudong_ng <tu_dong@allgrow-labo.jp>
2020-02-27 14:14:12 +0700

    Update 1.0.4

--- app/Providers/PermissionsServiceProvider.php
+++ app/Providers/PermissionsServiceProvider.php
@@ -25,10 +25,10 @@ class PermissionsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Permission::get()->map(function($permission){
-            Gate::define($permission->slug, function($user) use ($permission){
-               return $user->hasPermission($permission);
-            });
-        });
+        // Permission::get()->map(function($permission){
+        //     Gate::define($permission->slug, function($user) use ($permission){
+        //        return $user->hasPermission($permission);
+        //     });
+        // });
     }
 }
